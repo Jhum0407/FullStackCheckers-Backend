8f8e069ff5d58917da38e2eae344fc713a340e08
Toonces1980 <anthony.pajerowski@gmail.com>
2017-03-10 14:40:54 -0500

    changed gamepiece to extend arraylist

--- src/main/java/eightbitsakabigbyte/Entity/GamePieces.java
+++ src/main/java/eightbitsakabigbyte/Entity/GamePieces.java
@@ -2,25 +2,24 @@ package eightbitsakabigbyte.Entity;
 
 import java.util.ArrayList;
 
-public class GamePieces {
-    private ArrayList<GamePiece> currentPieces = new ArrayList<GamePiece>();
+public class GamePieces extends ArrayList<GamePiece>{
+
     private int pieceCounter = 1;
 
     public GamePieces(){
-        createGamePieces();
     }
 
-    private void createGamePieces() {
+    public void createGamePieces(){
         for (int row = 0; row < 3; row++) {
             for (int column = 0; column < 8; column++) {
                 if (column % 2 != 0 && row % 2 == 0) {
                     GamePiece piece = new GamePiece(false, pieceCounter,row,column);
-                    currentPieces.add(piece);
+                    add(piece);
                     pieceCounter++;
                 }
                 if (row == 1 && column % 2 == 0) {
                     GamePiece piece = new GamePiece(false, pieceCounter,row,column);
-                    currentPieces.add(piece);
+                    add(piece);
                     pieceCounter++;
                 }
             }
@@ -29,25 +28,15 @@ public class GamePieces {
             for (int column = 0; column < 8; column++) {
                 if (column % 2 == 0 && row % 2 != 0) {
                     GamePiece piece = new GamePiece(false, pieceCounter,row,column);
-                    currentPieces.add(piece);
+                    add(piece);
                     pieceCounter++;
                 }
                 if (row == 6 && column % 2 != 0) {
                     GamePiece piece = new GamePiece(false, pieceCounter,row,column);
-                    currentPieces.add(piece);
+                    add(piece);
                     pieceCounter++;
                 }
             }
         }
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for ( int i = 0; i < currentPieces.size(); i++) {
-            sb.append(currentPieces.get(i).getIdentifier() + " " +  currentPieces.get(i).getRow() + " "
-                    + currentPieces.get(i).getColumn() + " " + currentPieces.get(i).getIsKing() + "\n");
-        }
-        return sb.toString();
     }
 }
--- src/main/java/eightbitsakabigbyte/Service/GameService.java
+++ src/main/java/eightbitsakabigbyte/Service/GameService.java
@@ -1,94 +1,25 @@
 package eightbitsakabigbyte.Service;
 
+import eightbitsakabigbyte.Entity.GamePiece;
+import eightbitsakabigbyte.Entity.GamePieces;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class GameService {
+    public GamePieces gamePieces = new GamePieces();
+    {
+        gamePieces.createGamePieces();
+    }
 
-//    private GameBoard board = new GameBoard();
-//    private GamePiece emptyPiece;
-//    private GamePiece replacePiece;
-//    private MoveRequest move = new MoveRequest();
-//    {
-//        emptyPiece = new GamePiece("empty");
-//        emptyPiece.setIdentifier(-1);
-//    }
-//
-//
-//    //Instantiate one piece at [0][1] (upper left corner) for the MVP
-//    public GameBoard MVPboard(){
-//        for (int row = 0; row < board.getGameBoard().length; row++ ) {
-//            for (int column = 0; column < board.getGameBoard().length; column++) {
-//                board.getGameBoard()[row][column] = emptyPiece;
-//            }
-//        }
-//        board.getGameBoard()[0][1] = new GamePiece("black");
-//        return board;
-//    }
-//
-//    public GameBoard redrawGameBoard() {
-//        return null;
-//    }
-//
-//    public GameBoard redrawGameBoard(GamePiece gamePiece, int[][] moveFrom, int[][] moveTo){
-//
-//        return null;
-//    }
-//
-//    //Create a method to redraw the board once moves are made
-//    public GameBoard movePiece(MoveRequest request) {
-//
-//        for (int i = 0; i < board.getGameBoard().length; i++) {
-//            for (int j = 0; j < board.getGameBoard().length; j++) {
-//                if (board.getGameBoard()[i][j].getIdentifier() == request.getId()){
-//                    board.getGameBoard()[i][j] = emptyPiece;
-//                }
-//                if (request.getId() < 13) {
-//                    replacePiece = new GamePiece("red");
-//                    replacePiece.setIdentifier(request.getId());
-//                    board.getGameBoard()[request.getRow()][request.getColumn()] = replacePiece;
-//                    replacePiece.setRow(request.getRow());
-//                    replacePiece.setColumn(request.getColumn());
-//                } else replacePiece = new GamePiece("black");
-//                replacePiece.setIdentifier(request.getId());
-//                board.getGameBoard()[request.getRow()][request.getColumn()] = replacePiece;
-//                replacePiece.setRow(request.getRow());
-//                replacePiece.setColumn(request.getColumn());
-//            }
-//        }
-//        return board;
-//    }
-//
-//
-//    public int removePiece() {
-//            return -1;
-//        }
-//
-//    public boolean legalMove() {
-//        return true;
-//    }
-//
-//    public GameBoard getBoard() {
-//        return board;
-//    }
-//
-//    public void setBoard(GameBoard board) {
-//        this.board = board;
-//    }
-//
-//    public GamePiece getEmptyPiece() {
-//        return emptyPiece;
-//    }
-//
-//    public void setEmptyPiece(GamePiece emptyPiece) {
-//        this.emptyPiece = emptyPiece;
-//    }
-//
-//    public MoveRequest getMove() {
-//        return move;
-//    }
-//
-//    public void setMove(MoveRequest move) {
-//        this.move = move;
-//    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for ( int i = 0; i < gamePieces.size(); i++) {
+            sb.append(gamePieces.get(i).getIdentifier() + " " +  gamePieces.get(i).getRow() + " "
+                    + gamePieces.get(i).getColumn() + " " + gamePieces.get(i).getIsKing() + "\n");
+        }
+        return sb.toString();
+    }
 }
